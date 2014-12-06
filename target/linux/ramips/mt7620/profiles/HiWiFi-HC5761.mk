12104f051c02f2b6d1846d7673b93b56e3b8b026
wilkieliu <wilkieliu##163.com>
2014-12-06 22:54:22 +0800

    hc5661: add wireless usb driver

--- target/linux/ramips/mt7620/profiles/HiWiFi-HC5761.mk
+++ target/linux/ramips/mt7620/profiles/HiWiFi-HC5761.mk
@@ -13,8 +13,11 @@ define Profile/HiWiFi-HC5761
    block-mount mountd kmod-fs-ext4 ppp-mod-pppoe e2fsprogs \
    kmod-ledtrig-usbdev \
    kmod-fs-vfat fdisk ntfs-3g blkid \
-   openssh-sftp-server \
-   nfs-kernel-server nfs-kernel-server-utils kmod-loop portmap
+   kmod-rtl8192cu kmod-rt2800-usb kmod-net-rtl8188eu kmod-carl9170 \
+   kmod-ath9k-htc kmod-brcmfmac \
+   openssh-sftp-server vsftpd-tls \
+   nfs-kernel-server nfs-kernel-server-utils kmod-loop portmap \
+   6in4 6to4
 endef
 
 define Profile/HiWiFi-HC5761/Description
